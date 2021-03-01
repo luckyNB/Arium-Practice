b8c1671b051a0bf8fca993d92d14f598e715d851
Laxman Bhosle <laxmanbhosle@Laxmans-MBP.domain.name>
2021-03-01 15:40:55 +0530

    Added framework

---
+++ Assets/AriumFramework/Plugins/UnityCore/Extensions/TextUtils.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AriumFramework.Plugins.UnityCore.Extensions
+{
+    public static class TextUtils
+    {
+        public static string GetText(this GameObject gameObject)
+        {
+            return new GameObjectWrapper(gameObject).GetComponent<Text>().text;
+        }
+        public static bool IsGameObejectColliderEnabled(this GameObject gameObject)
+        {
+            return new GameObjectWrapper(gameObject).GetComponent<BoxCollider>().enabled;
+        }
+        
+        public static string GetImageSourceName(this GameObject gameObject)
+        {
+            return new GameObjectWrapper(gameObject).GetComponent<Image>().sprite.name;
+        }
+        
+        public static float GetGameObjectWidth(this GameObject gameObject)
+        {
+            return GetGameObjectSize(gameObject).width;
+        }
+        
+        public static float GetGameObjectHeight(this GameObject gameObject)
+        {
+            return GetGameObjectSize(gameObject).height;
+        }
+        
+        public static Rect GetGameObjectSize(GameObject gameObject)
+        {
+            return gameObject.GetComponent<RectTransform>().rect;
+        }
+    }
+}

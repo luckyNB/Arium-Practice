87a77c6716ff8cf595273ccb56a98ea0d56ed348
Laxman Bhosle <laxmanbhosle@Laxmans-MBP.domain.name>
2021-03-02 14:00:59 +0530

    Added test for moving player at different locations

--- Assets/Tests/AriumTests.cs
+++ Assets/Tests/AriumTests.cs
@@ -8,12 +8,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 namespace Tests
 {
-    public class AriumTests
+    public class AriumTests 
     {
         
         private Arium _arium;
+  
+
+       
 
         [OneTimeSetUp]
         public void SetUp()
@@ -26,19 +31,84 @@ namespace Tests
 
         
         
-        [UnityTest]
-        public IEnumerator ShouldDestroyObject()
-        {
-            var nameOfObjectToDestroy = "Player";
-            var objectToDestroy = _arium.FindGameObject(nameOfObjectToDestroy);
-            Object.Destroy(objectToDestroy);
-            yield return null;
-           // Assert.IsNull(_arium.FindGameObject("Player"));
-
-            // Assert.Throws<null>(() => _arium.FindGameObject(nameOfObjectToDestroy));
-        }
-        
-
      
+      
+
+   
+            [UnityTest]
+            public IEnumerator ShouldMoveToPositionForwardSide()
+            {
+                const float force = 82;
+
+                Transform playertr = _arium.GetComponent<Transform>("Player");
+                // yield return new WaitForSeconds(2);
+                UnityPushObject.Force=Vector3.forward*force;
+                _arium.PerformAction(new UnityPushObject(), "Player");
+                yield return new WaitForSeconds(7);
+
+                float distance = Vector3.Distance(playertr.position, 
+                    Vector3.zero);
+    
+                Assert.IsTrue(distance > 2f);
+                Assert.AreEqual( 0.5f,playertr.position.y);
+
+            }  
+            [UnityTest]
+            public IEnumerator ShouldMoveToPositionBackSide()
+            {
+                const float force = 82;
+
+                Transform playertr = _arium.GetComponent<Transform>("Player");
+                // yield return new WaitForSeconds(2);
+                UnityPushObject.Force=Vector3.back*force;
+                _arium.PerformAction(new UnityPushObject(), "Player");
+                yield return new WaitForSeconds(7);
+
+                float distance = Vector3.Distance(playertr.position, 
+                    Vector3.zero);
+    
+                Assert.IsTrue(distance > 2f);
+                Assert.AreEqual( 0.5f,playertr.position.y);
+
+            }   
+            [UnityTest]
+            public IEnumerator ShouldMoveToPositionleftSide()
+            {
+                const float force = 82;
+
+                Transform playertr = _arium.GetComponent<Transform>("Player");
+                // yield return new WaitForSeconds(2);
+                UnityPushObject.Force=Vector3.left*force;
+                _arium.PerformAction(new UnityPushObject(), "Player");
+                yield return new WaitForSeconds(7);
+
+                float distance = Vector3.Distance(playertr.position, 
+                    Vector3.zero);
+    
+                Assert.IsTrue(distance > 2f);
+                Assert.AreEqual( 0.5f,playertr.position.y);
+
+            }   
+            [UnityTest]
+            public IEnumerator ShouldMoveToPositionRightSide()
+            {
+                const float force = 82;
+
+                Transform playertr = _arium.GetComponent<Transform>("Player");
+                // yield return new WaitForSeconds(2);
+                UnityPushObject.Force=Vector3.right*force;
+                _arium.PerformAction(new UnityPushObject(), "Player");
+                yield return new WaitForSeconds(7);
+
+                float distance = Vector3.Distance(playertr.position, 
+                    Vector3.zero);
+    
+                Assert.IsTrue(distance > 2f);
+                Assert.AreEqual( 0.5f,playertr.position.y);
+
+            }   
+
+            
+
     }
 }
